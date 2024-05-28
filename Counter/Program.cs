d3366c0671604a2fc1b3983721a63ac8111413fb
Callum Cunningham <ccunningham@redev.co.uk>
2024-05-28 16:38:34 +0100

    Add project files.

---
+++ Counter/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = 1000;
+
+while (true)
+{
+    if (number > 0)
+    {
+        Console.WriteLine("Techno Time\n");
+        Thread.Sleep(number);
+
+        number -= 20;
+
+        Console.Clear();
+    }
+}
